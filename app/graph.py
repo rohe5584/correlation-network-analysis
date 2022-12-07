70f8f34abdceff338442425d314f071979f99365
rhellums <rhellums@smartasset.com>
2022-12-07 15:27:59 -0700

    Graphs for data and networkx code from sart

--- app/graph.py
+++ app/graph.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt
+import networkx as nx
 from typing import List
 import datetime
 from functools import reduce
@@ -14,7 +15,7 @@ class Graph:
         self.datasets = []
         self.filename = filename
         self.corr_vec = pd.DataFrame
-    
+        self.G = nx.Graph
     def buildDataSets(self):
         ## create a list from given test data file 
         ##traverse and create DataSet object, and extract data 
@@ -34,3 +35,72 @@ class Graph:
         ## finaly calculate correlation vector
         self.datasets = self.mergeLists()
         self.corr_vec = self.datasets.corr()
+    
+    def createNetwork(self)->nx.Graph:
+        correlation_matrix = self.corr_vec
+        nodes = correlation_matrix.index.values
+        correlation_matrix = np.asmatrix(correlation_matrix)
+
+        self.G = nx.from_numpy_matrix(correlation_matrix)
+        self.G = nx.relabel_nodes(self.G, lambda i: nodes[i])
+        self.G.edges(data=True)
+        for e in self.G.edges():
+            if(e[0] == e[1]):
+                self.G.remove_edge(e[0], e[1])
+        return self.G
+
+    def visualizeNetwork(self, G, correlation_direction):
+        # fig,ax = plt.subplots(1, figsize=(15,15))
+
+        #VISUALIZE THE WHOLE NETWWORK IN SPRING LAYOUT (will seperate a "root" node)
+        plt.figure(figsize=(8,8))
+        network = G
+
+        ##caluclate node sizes -> we want nodes with higher degrees to be bigger
+        #node_degrees = nx.degree(G)
+        degrees = [val for (node, val) in network.degree()]
+        node_sizes = [d**3 for d in degrees]
+   
+        edge, weights = zip(*nx.get_edge_attributes(network, 'weight').items())
+        positions = nx.spring_layout(network, k=3.0)
+        nx.draw_networkx_nodes(network, positions, node_color='#DA70D6', node_size=node_sizes, alpha=.8)
+        nx.draw_networkx_labels(network, positions, font_size=8, font_family='sans-serif')
+        if(correlation_direction == 0):
+            edges_color = plt.cm.GnBu
+            nx.draw_networkx_edges(network, positions, edgelist=edge, style='solid', width =weights, edge_color=weights, edge_cmap=edges_color, edge_vmin = min(weights), edge_vmax=max(weights))
+        elif(correlation_direction == 1):
+            edges_color = plt.cm.PuRd
+            nx.draw_networkx_edges(network, positions, edgelist=edge, style='solid', width =weights, edge_color=weights, edge_cmap=edges_color, edge_vmin = min(weights), edge_vmax=max(weights))
+        else: 
+            nx.draw_networkx_edges(network, positions, edgelist=edge, style='solid')
+
+        plt.axis('off')
+        plt.savefig("network_vis.png", format="PNG")
+        plt.show()
+    
+    def separateCorrelations(self, network, correlation_direction, min_correlation):
+        #we seperate the correlations between positive and negative correlations
+        #correlation direction <- either 0 or 1 where (0=positive) and (1=negative)
+
+        #rewire graph for only positive correlations
+        #i.e. delete all negative edge correlations
+        G_seperated = network.copy()
+        for index_1, index_2, weight in network.edges.data('weight'):
+            if(correlation_direction == 0):
+                if(weight < 0.0 or weight < min_correlation):
+                    G_seperated.remove_edge(index_1, index_2)
+            if(correlation_direction == 1):
+                if(weight >= 0.0 or weight > min_correlation):
+                    G_seperated.remove_edge(index_1, index_2)
+        return G_seperated
+
+    def graph_weighted_edges(self, network, correlation_direction):
+        G_weighted = network.copy()
+        for index_1, index_2, weight in network.edges.data('weight'):
+            if(correlation_direction == 1):
+                new_weight = -1*(1+abs(weight))**2
+            elif(correlation_direction == 0):
+                new_weight = (1+abs(weight))**2   
+            G_weighted.remove_edge(index_1, index_2)
+            G_weighted.add_edge(index_1, index_2, weight=new_weight)
+        return G_weighted
--- app/prod.py
+++ app/prod.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt
+import networkx as nx
 from typing import List
 import datetime
 from clean import *
@@ -12,7 +13,17 @@ graph = Graph(sys.argv[1],"2012-12-01","2022-09-01")
 graph.buildDataSets()
 graph.calcCorellationVectors()
 print(graph.corr_vec)
-
-#plt.plot(graph.datasets["cBond"].index,graph.datasets['cBond'])
-#plt.plot(graph.datasets["epi"].index,graph.datasets['epi'])
-#plt.show()
+graph.createNetwork()
+print(graph.G)
+#graph.visualizeNetwork(graph.G)
+print(type(graph.corr_vec))
+print(type(graph.G))
+positive_corr_network = graph.separateCorrelations(graph.G, 0, 0.7)
+negative_corr_network = graph.separateCorrelations(graph.G, 1, -0.1)
+print(positive_corr_network)
+print(negative_corr_network)
+positive_weighted_corr_network = graph.graph_weighted_edges(positive_corr_network, 0)
+negative_weighted_corr_network = graph.graph_weighted_edges(negative_corr_network, 1)
+graph.visualizeNetwork(negative_weighted_corr_network, 1)
+graph.visualizeNetwork(positive_weighted_corr_network, 0)
+#graph.visualizeNetwork(negative_corr_network)
